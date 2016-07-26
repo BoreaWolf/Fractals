cd2cbb8b18df7472564403864c8906effe93d125
BoreaWolf <ricky.orizio@gmail.com>
2016-07-26 11:25:58 +0200

    Splitting
    Trying to split lines: comparisons between points might be needed to correctly
    set the signs for the new coordinates computations.
    ⁽⁽◝(ˊʂ˴⁎)◞՞

--- Line.rb
+++ Line.rb
@@ -26,9 +26,66 @@ class Line
 	end
 
 	# Methods
+	# Drawing the line as defined
 	def draw( app )
 		app.line @a.x, @a.y, @b.x, @b.y
 		#app.shape
+	end
+
+	# Splitting the current line into 4 sub-lines
+	#  - Divide the line into 3 equal segments
+	#  - Draw an equilateral triangle on the middle segment
+	#  - Delete the base line of the triangle
+	#
+	# A         B
+	# o---------o
+	#
+	#       E
+	#       o
+	#      / \
+	# o---o  o---o
+	# A   C  D   B
+	#
+	#
+	def split()
+		# I need to find from the two points that define this line the set of 5
+		# points to generate all the lines required
+		#line_length = Math.sqrt( ( @a.x - @b.x ) ** 2 + ( @a.y - @b.y ) ** 2 )
+		#c = Point.new( @a.x + 3/line_length * (
+
+		dx = @a.x - @b.x
+		dy = @a.y - @b.y
+		line_length = Math.sqrt( dx ** 2 + dy ** 2 )
+		dx /= line_length
+		dy /= line_length
+
+		opposite = @a.y - @b.y
+		adiacent = @a.x - @b.x
+		hypo = Math.sqrt( opposite ** 2 + adiacent ** 2 )
+
+		#c = Point.new( @a.x + 3/line_length * dx, @a.y + 3/line_length * dy )
+		#d = Point.new( @a.x + 6/line_length * dx, @a.y + 6/line_length * dy )
+		#	c = Point.new( @a.x + line_length * 1/3, 
+		#				   @a.y + dy * line_length * 1/3 )
+
+		#	d = Point.new( @a.x + line_length * 2/3,
+		#				   @a.y + dy * line_length * 2/3 )
+
+		c = Point.new( @a.x + line_length/3 * Math.cos( adiacent / hypo ),
+					   @a.y + line_length/3 * Math.sin( opposite / hypo ) )
+		
+		d = Point.new( @a.x + line_length * 2/3 * Math.cos( adiacent / hypo ),
+					   @a.y + line_length * 2/3 * Math.sin( opposite / hypo ) )
+
+		# Knowing that it is an equilater triangle, I know its angle
+		e = Point.new( c.x + line_length/3 * Math.cos( Math::PI/3 ),
+					   c.y + line_length/3 * Math.sin( Math::PI/3 ) )
+
+		# Creating the new lines from these points
+		Line.new( @a, c )
+		Line.new( c, e )
+		Line.new( e, d )
+		Line.new( d, @b )
 	end
 	
 end
--- koch.rb
+++ koch.rb
@@ -8,36 +8,49 @@
 require_relative "./Line.rb"
 require_relative "./Point.rb"
 
+def draw_all( app )
+	Line.all_instances.each{ |x| x.draw( app ) }
+	Point.all_instances.each{ |x| x.draw( app ) }
+end
+
 Shoes.app :width => 512, :height => 512 do
 	background green
 
-	@snowflake = flow :width => "100%", height => "100%", margin => 10 do
-		background blue
+	#	stack :height => 88 do
+	#		@intro = para "This will do!"
+	#		@intro2 = para "At each click of the mouse you will procede to the next step"
+	#	end
 
-		#	stack :height => 88 do
-		#		@intro = para "This will do!"
-		#		@intro2 = para "At each click of the mouse you will procede to the next step"
-		#	end
+	# Starting points
+	@a = Point.new( 10, (512-10*2)/4 )
+	@b = Point.new( 512-10*2, (512-10*2)/4 )
+	@c = Point.new( (512-10*2)/2, (512-10*2) )
+	Line.new( @a, @b )
+	Line.new( @b, @c )
+	Line.new( @c, @a )
 
-		# Starting points
-		@a = Point.new( 10, (512-10*2)/4 )
-		@b = Point.new( 512-10*2, (512-10*2)/4 )
-		@c = Point.new( (512-10*2)/2, (512-10*2) )
-		Line.new( @a, @b )
-		Line.new( @b, @c )
-		Line.new( @c, @a )
+	#stack do
+	#	background white
+	Line.all_instances.each{ |x| x.draw( self ) }
+	Point.all_instances.each{ |x| x.draw( self ) }
+	#end
+	
+	# Event: next step of the snowflake
+	#@intro.click{ @intro2.replace "Sbra" }
+		#	Line.all_instances
 
-		stack do
-			background white
-			Line.all_instances.each{ |x| x.draw( self ) }
-			Point.all_instances.each{ |x| x.draw( self ) }
-		end
-
-		# Event: next step of the snowflake
-		#@intro.click{ @intro2.replace "Sbra" }
-			#	Line.all_instances
+	click do
+		alert( "Sbra" )
+		# Splitting the lines as requested to generate the fractal
+		# Saving the current lines so I don't fall into an infinite loop
+		#	current_lines = Line.all_instances
+		#	current_lines.each{ |x| x.split }
+		Line.all_instances[0].split
+		#Line.all_instances[1].split
+		#Line.all_instances[2].split
+		# Drawing every line known
+		# NB Probably I have to delete the previous lines
+		draw_all( self )
 	end
-
-	@snowflake.click{ alert( "sbra" ) }
 
 end
